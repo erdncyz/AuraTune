1419d28f760af27ffa4c9689a228df6c8f7fea06
Erdinç Yılmaz <erdinc.yilmaz@digiturk.com.tr>
2026-04-23 18:39:56 +0300

    Initial Commit

---
+++ AuraTune/AuraTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraTuneApp.swift
+//  AuraTune
+//
+//  Created by Erdinç Yılmaz on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct AuraTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
